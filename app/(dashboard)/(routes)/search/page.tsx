bd83dd68bef41477627426ac25a6b14791999d59
Mazen Abdalla <92177398+MazenAbdallaAbdelhadi@users.noreply.github.com>
2024-08-26 18:49:54 +0300

    course search & courses list

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,5 +1,40 @@
-const SearchPage = () => {
-  return <div>Search Page</div>;
+import { db } from "@/lib/db";
+import Categories from "./_components/categories";
+import { SearchInput } from "@/components/search-inpute";
+import { getCourses } from "@/actions/get-courses";
+import { currentUser } from "@/lib/auth";
+import { CoursesList } from "@/components/courses-list";
+
+interface SearchPageProps {
+  searchParams: {
+    title: string;
+    categoryId: string;
+  };
+}
+
+const SearchPage = async ({ searchParams }: SearchPageProps) => {
+  const user = await currentUser();
+
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
+
+  const courses = await getCourses({ userId: user?.id!, ...searchParams });
+
+  return (
+    <>
+      <div className="px-6 pt-6 md:hidden md:mb-0 block">
+        <SearchInput />
+      </div>
+      <div className="p-6 space-y-4">
+        <Categories items={categories} />
+
+        <CoursesList items={courses} />
+      </div>
+    </>
+  );
 };
 
 export default SearchPage;
