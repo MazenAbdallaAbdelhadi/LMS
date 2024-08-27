6dccc299c78c260666b90f375b6551de76359081
Mazen Abdalla <92177398+MazenAbdallaAbdelhadi@users.noreply.github.com>
2024-08-27 20:54:54 +0300

    student dashboard

--- app/(dashboard)/(routes)/(root)/page.tsx
+++ app/(dashboard)/(routes)/(root)/page.tsx
@@ -1,5 +1,35 @@
-const HomePage = () => {
-  return <div>This is Home Page</div>;
+import { getDashboardCourses } from "@/actions/get-dashboard-courses";
+import { CoursesList } from "@/components/courses-list";
+import { currentUser } from "@/lib/auth";
+import { CheckCircle, Clock } from "lucide-react";
+import { InfoCard } from "./_components/info-card";
+
+const Dashboard = async () => {
+  const user = await currentUser();
+
+  const { completedCourses, coursesInProgress } = await getDashboardCourses(
+    user?.id!
+  );
+
+  return (
+    <div className="p-6 space-y-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        <InfoCard
+          icon={Clock}
+          label="In Progress"
+          numberOfItems={coursesInProgress.length}
+        />
+        <InfoCard
+          icon={CheckCircle}
+          label="Completed"
+          numberOfItems={completedCourses.length}
+          variant="success"
+        />
+      </div>
+
+      <CoursesList items={[...coursesInProgress, ...completedCourses]} />
+    </div>
+  );
 };
 
-export default HomePage;
+export default Dashboard;
