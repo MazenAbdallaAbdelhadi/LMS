8df03f9c2ff68eaefe2e5f219f4c069528129b2e
Mazen Abdalla <92177398+MazenAbdallaAbdelhadi@users.noreply.github.com>
2024-09-17 20:29:05 +0300

    use nodemailer for sending emails

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,36 +1,67 @@
-import { Resend } from "resend";
+import nodemailer from "nodemailer";
+// import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// don't have a domain to use resend
+// const resend = new Resend(process.env.RESEND_API_KEY);
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.MAIL_USERNAME,
+    pass: process.env.MAIL_PASS,
+  },
+});
 
 const domain = process.env.NEXT_PUBLIC_APP_URL;
 
 export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
-  await resend.emails.send({
-    from: "Acme <onboarding@resend.dev>",
+  await transporter.sendMail({
+    from: "Acme <mazen.mezoo20189@gmail.com>",
     to: email,
     subject: "2FA code",
     html: `<p>your 2FA code: ${token}</p>`,
   });
+
+  // await resend.emails.send({
+  //   from: "Acme <onboarding@resend.dev>",
+  //   to: email,
+  //   subject: "2FA code",
+  //   html: `<p>your 2FA code: ${token}</p>`,
+  // });
 };
 
 export const sendResetPasswordEmail = async (email: string, token: string) => {
   const resetPasswordLink = `${domain}/new-password?token=${token}`;
 
-  await resend.emails.send({
-    from: "Acme <onboarding@resend.dev>",
+  await transporter.sendMail({
+    from: "Acme <mazen.mezoo20189@gmail.com>",
     to: email,
     subject: "Reset your password",
     html: `<p>Click <a href=${resetPasswordLink}>here</a> to reset password.</p>`,
   });
+
+  // await resend.emails.send({
+  //   from: "Acme <onboarding@resend.dev>",
+  //   to: email,
+  //   subject: "Reset your password",
+  //   html: `<p>Click <a href=${resetPasswordLink}>here</a> to reset password.</p>`,
+  // });
 };
 
 export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/new-verification?token=${token}`;
 
-  await resend.emails.send({
-    from: "Acme <onboarding@resend.dev>",
+  await transporter.sendMail({
+    from: "Acme <mazen.mezoo20189@gmail.com>",
     to: email,
     subject: "Confirm your email",
     html: `<p>Click <a href=${confirmLink}>here</a> to confirm email.</p>`,
   });
+
+  // await resend.emails.send({
+  //   from: "Acme <onboarding@resend.dev>",
+  //   to: email,
+  //   subject: "Confirm your email",
+  //   html: `<p>Click <a href=${confirmLink}>here</a> to confirm email.</p>`,
+  // });
 };
